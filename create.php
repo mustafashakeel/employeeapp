5bfb264c505acc80ac29263a84627bef599f766b
Mike1 Qureshi <mustafashakeel@gmail.com>
2023-01-23 15:00:41 -0800

    added Create page

--- create.php
+++ create.php
@@ -1,39 +1,90 @@
 <?php
+// Include config file
 require_once "config.php";
 
+
+
+// echo $_SERVER['PHP_SELF'];    // Returns the filename of the currently executing script
+// echo $_SERVER['GATEWAY_INTERFACE'];    //Returns the version of the Common Gateway Interface (CGI) the server is using
+// echo  $_SERVER['SERVER_ADDR']; //	Returns the IP address of the host server
+// echo $_SERVER['SERVER_NAME'];    //Returns the name of the host server (such as www.w3schools.com)
+// echo $_SERVER['SERVER_SOFTWARE']; //	Returns the server identification string (such as Apache/2.2.24)
+// echo $_SERVER['SERVER_PROTOCOL']; //	Returns the name and revision of the information protocol (such as HTTP/1.1)
+// echo $_SERVER['REQUEST_METHOD']; //	Returns the request method used to access the page (such as POST)
+// echo $_SERVER['REQUEST_TIME'];    //Returns the timestamp of the start of the request (such as 1377687496)
+// echo $_SERVER['QUERY_STRING'];    //Returns the query string if the page is accessed via a query string
+// echo $_SERVER['HTTP_ACCEPT'];    //Returns the Accept header from the current request
+// echo $_SERVER['HTTP_ACCEPT_CHARSET'];    //Returns the Accept_Charset header from the current request (such as utf-8,ISO-8859-1)
+// echo $_SERVER['HTTP_HOST'];    //Returns the Host header from the current request
+// echo $_SERVER['HTTP_REFERER'];    //Returns the complete URL of the current page (not reliable because not all user-agents support it)
+// echo $_SERVER['HTTPS']; //	Is the script queried through a secure HTTP protocol
+// echo $_SERVER['REMOTE_ADDR']; //	Returns the IP address from where the user is viewing the current page
+
+// Define variables and initialize with empty values
+$name = $address = $salary = "";
+$name_err = $address_err = $salary_err = "";
+
+// Processing form data when form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if (empty($input_name)) {
+        $name_err = "Please enter a name.";
+    } elseif (!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[a-zA-Z\s]+$/")))) {
+        $name_err = "Please enter a valid name.";
+    } else {
+        $name = $input_name;
+    }
 
-    echo "  FROM POST ";
-    $name = trim($_POST["name"]);
-    $address = trim($_POST["address"]);
-    $salary = trim($_POST["salary"]);
+    // Validate address
+    $input_address = trim($_POST["address"]);
+    if (empty($input_address)) {
+        $address_err = "Please enter an address.";
+    } else {
+        $address = $input_address;
+    }
 
-    echo  $name;
-    echo   $address;
-    echo   $salary;
+    // Validate salary
+    $input_salary = trim($_POST["salary"]);
+    if (empty($input_salary)) {
+        $salary_err = "Please enter the salary amount.";
+    } elseif (!ctype_digit($input_salary)) {
+        $salary_err = "Please enter a positive integer value.";
+    } else {
+        $salary = $input_salary;
+    }
 
-    if (empty($name) && empty($address) && empty($salary)) {
-        $sql = "INSERT INTO employees (name, address, salary) VALUES (?,?,?)";
+    // Check input errors before inserting in database
+    if (empty($name_err) && empty($address_err) && empty($salary_err)) {
+        // Prepare an insert statement
+        $sql = "INSERT INTO employees (name, address, salary) VALUES (?, ?, ?)";
 
-        if ($stmt =  mysqli_prepare($link, $sql)) {
-            // Bind the variables to the parameters
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "sss", $param_name, $param_address, $param_salary);
 
+            // Set parameters
             $param_name = $name;
             $param_address = $address;
             $param_salary = $salary;
 
+            // Attempt to execute the prepared statement
             if (mysqli_stmt_execute($stmt)) {
-                echo " Record Added ";
+                // Records created successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
             } else {
-                echo " Something Went Wrong ";
+                echo "Oops! Something went wrong. Please try again later.";
             }
-
-            echo "TTTTT ";
         }
-    }
-}
 
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+
+    // Close connection
+    mysqli_close($link);
+}
 ?>
 
 <!DOCTYPE html>
@@ -41,52 +92,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper {
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
 </head>
-<style>
-    .wrapper {
-        width: 600px;
-        margin: 0 auto;
-    }
-</style>
 
 <body>
     <div class="wrapper">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <h3>Add Employee </h3>
+                    <h2 class="mt-5">Create Record</h2>
+                    <p>Please fill this form and submit to add employee record to the database.</p>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="form-group">
-                            <label for="name">Name</label>
-                            <input type="text" class="form-control" id="name" name="name" aria-describedby="name" placeholder="Enter name">
-                            <small id="nameHelp" class="form-text text-muted">Enter your name.</small>
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
+                            <span class="invalid-feedback"><?php echo $name_err; ?></span>
                         </div>
                         <div class="form-group">
-                            <label for="address">Address</label>
-                            <textarea class="form-control" id="address" name="address" rows="3"></textarea>
+                            <label>Address</label>
+                            <textarea name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $address_err; ?></span>
                         </div>
                         <div class="form-group">
-                            <label for="salary">Salary</label>
-                            <input type="text" class="form-control" id="salary" name="salary" aria-describedby="name" placeholder="Enter Salary">
-                            <small id="salaryHelp" class="form-text text-muted">Enter your Salary.</small>
+                            <label>Salary</label>
+                            <input type="text" name="salary" class="form-control <?php echo (!empty($salary_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $salary; ?>">
+                            <span class="invalid-feedback"><?php echo $salary_err; ?></span>
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
                     </form>
-
                 </div>
             </div>
-
-
         </div>
     </div>
-
-
-
 </body>
 
 </html>
